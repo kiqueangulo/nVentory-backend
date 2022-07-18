fceb0e88cb74071150a1750b8f539b3592cc2365
collinblank <cablank25@gmail.com>
2022-07-18 10:54:20 -0400

    updated server.js to index.js and added Jays updates back

---
+++ index.js
@@ -0,0 +1,30 @@
+//DEPENDENCIES
+const express = require("express");
+const app = express();
+
+
+//LISTEN
+app.listen(3000, () => {
+  console.log('server is listening');
+});
+
+
+//DIRECTIONS FOR SETTING UP HEROKU HERE.
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// client.connect();
+
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
