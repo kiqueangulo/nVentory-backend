8791a829143adff2610fe775059d87837811dbbc
Jay Brewer <97116059+jaybrewer49@users.noreply.github.com>
2022-07-20 17:10:55 -0400

    completed products controller

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -1,7 +1,7 @@
 //DEPENDENCIES
 const products = require('express').Router()
 const db = require('../models')
-const { location, product_details, product_location } = db 
+const {  product_details } = db 
 const { Op } = require('sequelize')
 
 // FIND ALL BANDS
@@ -16,6 +16,64 @@ products.get('/', async (req, res) => {
         res.status(200).json(foundProducts)
     } catch (error) {
         res.status(500).json(error)
+    }
+})
+
+//FIND A PRODUCT
+products.get('/:name', async (req, res) => {
+    try {
+        const foundBand = await product_details.findOne({
+            where: { name: req.params.name },
+        })
+
+        res.status(200).json(foundProducts)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//CREATE PRODUCT
+products.post('/', async (req, res) => {
+    try {
+        const newProduct = await product_details.create(req.body)
+        res.status(200).json({
+            message: 'New product added to nVentory',
+            data: newProduct
+        }) 
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+//UPDATE PRODUCT
+products.put('/:id', async (req, res) => {
+   try {
+    const updateProducts = await product_details.update(req.body, {
+        where: {
+            product_id: req.params.id
+        }
+    })
+    res.status(200).json({
+        message: `Updated ${updateProducts} within nVentory`
+    })
+   } catch (error) {
+    res.status(500).json(err)
+   }
+})
+
+//DELETE PRODUCTS
+products.delete('/:id', async (req, res) => {
+    try {
+        const deleteProducts = await product_details.destroy({
+            where: {
+                product_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Deleted ${deleteProducts} within nVentory`
+        })
+    } catch (error) {
+        res.status(500).json(err)
     }
 })
 
