68a2df80285e2e02ea464670687a42b6d4cc41d2
Jay <jaybrewer@Jays-MacBook-Pro.local>
2022-07-23 14:11:20 -0400

    fixed find all location bug

--- controllers/locations_controller.js
+++ controllers/locations_controller.js
@@ -1,18 +1,17 @@
-
+//DEPENDENCIES
 const locations = require('express').Router()
 const db = require('../models')
-const { location } = db
+const {  location } = db 
 const { Op } = require('sequelize')
 
-// LIST ALL LOCATIONS
+// LIST ALL PRODUCTS
 locations.get('/', async (req, res) => {
     try {
-        const foundLocation = await locations.findAll()
+        const foundLocation = await location.findAll()
         console.log(foundLocation)
         res.status(200).json(foundLocation)
     } catch (error) {
         res.status(500).json(error)
     }
 })
-
 module.exports = locations;
