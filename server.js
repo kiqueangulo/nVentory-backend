9ef19648fec8d67172f57bd171fef6db5e613c46
collinblank <cablank25@gmail.com>
2022-07-23 11:21:31 -0400

    commented out order in get call

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 //DEPENDENCIES
 const express = require("express");
-const db = require('./models/index.js');
-const { Sequelize } = require('sequelize')
 const app = express();
+const { Sequelize } = require('sequelize')
+const db = require('./models/index.js');
+
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config();
@@ -25,25 +27,3 @@ app.use('/products', productsController)
   app.listen(process.env.PORT, () => {
     console.log(`server is listening on port: ${process.env.PORT}`);
   });
-
- 
-
-//DIRECTIONS FOR SETTING UP HEROKU HERE.
-// const { Client } = require('pg');
-
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-
-// client.connect();
-
-// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-//   if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   client.end();
-// });
